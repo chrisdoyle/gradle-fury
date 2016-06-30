0a4ba1fa4215357a7f35ac825ec5eb146fc4e86c
Alex <alexoree@apache.org>
2016-06-29 20:02:04 -0400

    #12 should fix the ci bits

--- verify.sh
+++ verify.sh
@@ -3,20 +3,23 @@
 version=1.0.5-SNAPSHOT
 
 declare -a arr=("~/.m2/repository/com/chrisdoyle/hello-world-aar/$version/hello-world-aar-$version-debug.aar" \
-     "~/.m2/repository/com/chrisdoyle/hello-world-aar/$version/hello-world-aar-$version-debug.pom" \
+     "~/.m2/repository/com/chrisdoyle/hello-world-aar/$version/hello-world-aar-$version.pom" \
       "~/.m2/repository/com/chrisdoyle/hello-world-lib/$version/hello-world-lib-$version.jar" \
       "~/.m2/repository/com/chrisdoyle/hello-world-lib/$version/hello-world-lib-$version.pom" \
       "~/.m2/repository/com/chrisdoyle/hello-world-apk/$version/hello-world-apk-$version-barDebug.apk" \
       "~/.m2/repository/com/chrisdoyle/hello-world-apk/$version/hello-world-apk-$version-bazDebug.apk" \
       "~/.m2/repository/com/chrisdoyle/hello-world-apk/$version/hello-world-apk-$version-fooDebug.apk" \
-      "~/.m2/repository/com/chrisdoyle/hello-world-apk/$version/hello-world-apk-$version.pom" \
-      )
- 
+      "~/.m2/repository/com/chrisdoyle/hello-world-apk/$version/hello-world-apk-$version.pom" )
+
 
 for i in "${arr[@]}"
 do
-    if [[ ! -f "$i"  ]] ; then
-        echo 'File "$i" is not there, aborting.'
+    echo "Testing for $i"
+    if [ ! -f "`eval echo ${i//>}`"  ] ; then
+        echo "File $i is not there, aborting."
         exit 1
+
+    else
+       echo "$i" "OK";
     fi
 done
