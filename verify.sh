999f9d91a273fe1bdd6c1bb6a150a9ace03e1889
Alex <alexoree@apache.org>
2016-06-30 07:19:34 -0400

    reads the version from gradle.properties

--- verify.sh
+++ verify.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
-version=1.0.5-SNAPSHOT
+#this file verifies that the expected artifacts are published to maven local
+
+#the version we're expecting
+version=""
+
+#the properties file that gradle uses
+PROPERTIES_FILE=gradle.properties
+
+
+# Reads property $2 from properties file $1 and echos the value. To call this method do:
+# src http://gothither.blogspot.com/2012/06/read-value-from-java-properties-file-in.html
+#     V=$(getProp filename property)
+#
+function getProp () {
+    # ignore lines with '#' as the first non-space character (comments)
+    # grep for the property we want
+    # get the last match just in case
+    # strip the "property=" part but leave any '=' characters in the value
+
+    echo `sed '/^[[:space:]]*\#/d' $1 | grep $2  | tail -n 1 | cut -d "=" -f2- | sed 's/^[[:space:]]*//;s/[[:space:]]*$//'`
+}
+
+# get the version number from the gradle build
+version=`eval getProp $PROPERTIES_FILE pom.version`
+
+
+# these are all the files we're testing for the existence of
 
 declare -a arr=("~/.m2/repository/com/chrisdoyle/hello-world-aar/$version/hello-world-aar-$version-debug.aar" \
      "~/.m2/repository/com/chrisdoyle/hello-world-aar/$version/hello-world-aar-$version.pom" \
@@ -23,3 +49,6 @@ do
        echo "$i" "OK";
     fi
 done
+
+
+echo "Test passed!"
